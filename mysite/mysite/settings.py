f8dfdd6a9f7d11fa876e8fcc369603b39f89f74c
Fiip3k <fikszyn3k@gmail.com>
2023-04-07 20:27:28 +0200

    that's what happens when you promise to finish one small thing before taking a break

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,10 +37,10 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+
 ]
 
 # Application definition
