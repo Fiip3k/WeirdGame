ba9082bdef78e8eb80b1ba756a9e001fc0e83a64
Fiip3k <fikszyn3k@gmail.com>
2023-04-07 19:24:26 +0200

    come on render work

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'mysite',
 ]
 
-if RENDER_EXTERNAL_HOSTNAME:
-    INSTALLED_APPS.append('render.apps.RenderConfig')
+# if RENDER_EXTERNAL_HOSTNAME:
+#    INSTALLED_APPS.append('render.apps.RenderConfig')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
