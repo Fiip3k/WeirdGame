56ab2f8f80014afce6fdee48852e49afddfed5a4
Fiip3k <fikszyn3k@gmail.com>
2023-04-07 19:19:00 +0200

    yet another

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+$ sudo apt install python3.11
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python mysite/manage.py collectstatic --no-input
